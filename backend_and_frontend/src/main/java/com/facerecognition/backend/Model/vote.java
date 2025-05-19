96f23f17b36ebc976abdd9a175cf7a3b169d9203
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2025-05-19 16:16:16 +0530

    Add files via upload

---
+++ backend_and_frontend/src/main/java/com/facerecognition/backend/Model/vote.java
@@ -0,0 +1,14 @@
+package com.collegeproject.backendandfrontend.Model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class vote {
+    
+ 
+    private String voterId;
+    private String candidate;
+}
