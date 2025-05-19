5e9055c5030e684fd47a11c7ff3982633a86d03b
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2025-05-19 18:12:16 +0530

    Add files via upload
    contain unit and integration tests for the project

---
+++ backend_and_frontend/src/test/java/com/facerecog/backend2/BackendandfrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeproject.backendandfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendandfrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
